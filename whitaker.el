fc8b47c9008127fc1a1dd2d4b2f5f83ff6d8ef70
Matus Goljer <dota.keys@gmail.com>
2014-06-05 00:44:35 +0200

    Replace ace-jump with better-jump

--- whitaker.el
+++ whitaker.el
@@ -21,6 +21,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(require 'dash)
+(require 'better-jump)
+
 (defgroup whitaker ()
   "Comint interface for Whitaker's words."
   :prefix "whitaker-")
@@ -112,23 +115,18 @@ This buffer is recognized by searching for buffer with name
     (whitaker t)
     (whitaker-send-input word-or-region)))
 
-(defvar whitaker--ace-jump-save-position nil
-  "Position to jump back to after word is sent to the whitaker
-buffer.")
-
-(defun whitaker--ace-jump-helper ()
-  (whitaker-send-input (word-at-point))
-  (goto-char whitaker--ace-jump-save-position)
-  (remove-hook 'ace-jump-mode-end-hook 'whitaker--ace-jump-helper))
-
 ;;;###autoload
-(defun whitaker-ace-jump (char)
-  "Jump to location by calling `ace-jump-mode', then send the
-word under point to whitaker buffer."
-  (interactive (list (read-char "Head Char:")))
-  (setq whitaker--ace-jump-save-position (point))
-  (ace-jump-word-mode char)
-  (add-hook 'ace-jump-mode-end-hook 'whitaker--ace-jump-helper))
+(defun whitaker-jump (char)
+  "Jump to location by using `better-jump', then send the word
+under point to whitaker buffer."
+  (interactive "cHead char: ")
+  (bjump-jump-cw
+   (bjump-selector-char char)
+   'bjump-picker-single-letter
+   (lambda (ov)
+     (save-excursion
+       (goto-char (ov-beg ov))
+       (whitaker-send-input (word-at-point))))))
 
 
 ;;; whitaker comint mode
